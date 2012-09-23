973139ba0deaa6f235dba71fd667cbb4ad6d53d4
xiu <xiu@xius-MacBook-Pro.local>
2012-09-23 19:38:01 +0800

    init

---
+++ Exercise1/Exercise1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Exercise1
+//
+//  Created by xiu on 9/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
