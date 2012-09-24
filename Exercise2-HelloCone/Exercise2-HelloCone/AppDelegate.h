227aeb4d10c328965133e6e4f2d72dc3b9d73415
suzhaoyang <xiuzhaoyang2004@163.com>
2012-09-24 11:17:07 +0800

    Exercise2 Vector.hpp

---
+++ Exercise2-HelloCone/Exercise2-HelloCone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Exercise2-HelloCone
+//
+//  Created by su_zhaoyang on 12-9-24.
+//  Copyright (c) 2012年 su_zhaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Exercise2-HelloCone/Exercise2-HelloCone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exercise2-HelloCone
+//
+//  Created by su_zhaoyang on 12-9-24.
+//  Copyright (c) 2012年 su_zhaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
